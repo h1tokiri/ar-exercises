2d35a8d13a40ec04263fcfe2053e20591b75e675
Javin Leslie Lau <javinlau21@gmail.com>
2025-05-05 16:55:42 -0400

    exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,19 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+# 2. Ask the user for a store name
+puts "Enter a store name:"
+@store_name = gets.chomp
+
+# 3. Attempt to create a store with the inputted name but leave out the other fields
+@store = Store.new(name: @store_name)
+
+# 4. Save the store and display any error messages
+if @store.save
+  puts "Store was saved successfully!"
+else
+  puts "Error(s):"
+  @store.errors.full_messages.each do |message|
+    puts "- #{message}"
+  end
+end
